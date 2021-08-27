90e1599d4a0ac87cbb35f3b7400f3b44fb5c7a3c
aligoodman <89408586+aligoodman@users.noreply.github.com>
2021-08-27 14:09:15 +0100

    Update AdvancedDynoControl.ino

--- DynoControl/AdvancedDynoControl.ino
+++ DynoControl/AdvancedDynoControl.ino
@@ -8,13 +8,16 @@ float rpm = 0;
 float bgrad = 1;
 float value;
 float max;
+int Mode;
 
 
 void setup() {
   
-  pinMode(3, OUTPUT);
+  pinMode(DAC0, OUTPUT);
   Serial.begin(9600);
   Serial.setTimeout(3000000);
+  analogWrite(DAC0, 0);
+  analogWriteResolution(12);
 }
 
 void loop() {
@@ -29,31 +32,36 @@ void loop() {
   while(Serial.available()== 0) {
     
   }
-  
-  if(Serial.parseInt() == "rpm" or "RPM") {
+  Mode = Serial.parseInt();
+  Serial.println(Mode);
+
     
-    
+  if(Mode == 1) {
     Serial.println("Request RPM?"); //Prompt User for Input
-  while (Serial.available() == 0) {
+    while (Serial.available() == 0) {
     // Wait for User to Input Data
-  }
-  rpm = Serial.parseInt(); //Read the data the user has input
-  voltage = (rpm/vgrad) + v0;
-  value = bgrad*(voltage);
-  analogWrite(3, value); 
-  }
+   }
+    rpm = Serial.parseInt(); //Read the data the user has input
+    voltage = (rpm/vgrad) + v0;
+    value = bgrad*(voltage);
+    analogWrite(DAC0, value); 
+  } 
   
-  if(Serial.parseInt() == "Ramp up" or "ramp up") {
+  if(Mode == 2) {
     Serial.println("Ramp to Input number?");
     //Will need conversion from rpm to input value
-    max = Serial.parseInt();
-    // need something to convert rpm to voltage signal
-    for (int i = 0; i <= max; i++) {
-    analogWrite(DAC0, i);
-    delay(10);
-    for (int i = 0; i <= max; i++) {
-    analogWrite(DAC0, (Max-i));
-    delay(10);
+  while (Serial.available() == 0){
+  }
+  max = Serial.parseInt();
+  // need something to convert rpm to voltage signal
+  for (int i = 0; i <= max; i++) {
+  analogWrite(DAC0, i);
+  delay(2);
+  }
+  for (int i = 0; i <= max; i++) {
+  analogWrite(DAC0, (max-i));
+  delay(2);
+  }
     
   }
 
