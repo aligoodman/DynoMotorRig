9362f740403e107b62daba2735e75455ecd2dd8d
Ali IPAD <ag941@cantab.ac.uk>
2021-09-03 10:53:48 +0100

    ?

--- DynoControl/InputTrack.ino
+++ DynoControl/InputTrack.ino
@@ -10,11 +10,14 @@ void setup() {
   Serial.setTimeout(3000000);
   analogWrite(DAC0, 0);
   analogWriteResolution(12);
+  analogReadResolution(12);
 }
 
 void loop() {
   
   track = analogRead(A0);
   analogWrite(DAC0, track)
+  
+  
   
 }
