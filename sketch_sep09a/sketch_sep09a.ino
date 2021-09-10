f434e70f7e126505da00c7f1372ba3eaf24a42cf
Ali Goodman <ag941@cantab.ac.uk>
2021-09-10 17:12:11 +0100

    added pwm signal control file

---
+++ sketch_sep09a/sketch_sep09a.ino
@@ -0,0 +1,63 @@
+#include <Servo.h>
+int input;
+float value;
+float sinSignal;
+Servo myServo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.setTimeout(100000);
+  Serial.begin(9600);
+  analogWriteResolution(12);
+  analogReadResolution(12);
+  myServo.attach(7);
+  myServo.writeMicroseconds(1500);
+}
+
+
+
+/*void loop() {
+
+  Serial.println("Enter a value between -100 and 100");
+  value = Serial.parseInt();
+  if( value>1000 or value<-1000){
+    Serial.println("Value out of range");
+  }
+  else {
+    input = 2048 + (value);
+    Serial.println(input);
+    analogWrite(7, input);  
+  }
+}
+*/
+
+
+void loop() {
+  Serial.println("Enter a value between -1000 and 1000");
+  value = Serial.parseInt();
+  if( value>1000 or value<-1000){
+    Serial.println("Value out of range");
+  }
+  else {
+    input = 2048 + (value);
+    Serial.println(input);
+    if(value > 0) {
+      for(int i = 0; i <= value; i++) {
+        analogWrite(7, 2048 + i);
+ 
+      }
+    }
+    if(value <= 0) {
+      for(int a = 0; a <= abs(value); a=a+1){
+        sinSignal = value*abs(sin((10*a/(abs(value)))*(3.14159)));
+        //analogWrite(7, 2048 + sinSignal);
+        //delay(200);
+        //Serial.println(sinSignal);
+        myServo.writeMicroseconds(1500+(sinSignal));
+        delay(20);
+      }
+     //analogWrite(7,0);
+    }
+    
+  }
+}
